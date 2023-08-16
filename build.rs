f2af32cb5ade4bfb36a71c30486e10afd71021d5
dommyrock <dominik.polzer@hotmail.com>
2023-08-16 12:58:47 +0200

    build.rs updated logic / check if graphviz is installed

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+//REF https://doc.rust-lang.org/cargo/reference/build-scripts.html
 extern crate chrono;
 
 use chrono::{Datelike, Local};
@@ -5,33 +6,31 @@ use std::fs;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
-//REF https://doc.rust-lang.org/cargo/reference/build-scripts.html
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=cargo.toml");
 
     let now = Local::now();
     let date_today = format!("{}_{}_{}", now.month(), now.day(), now.year());
     let depgraph_filename = format!("depGraph-{}.svg", date_today);
 
-    is_depgraph_installed();
+    let _ = are_build_deps_installed();
 
     // Run the post-build command
     let cargo = Command::new("cargo")
         .args(&["depgraph", "--all-deps"])
         .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to run cargo depgraph");
+        .spawn()?;
 
     let dot = Command::new("dot")
         .arg("-Tsvg")
         .stdin(cargo.stdout.unwrap())
-        .output()
-        .expect("failed to run dot");
+        .output()?;
 
     cleanup_old_depgraphs();
 
     //Latest dependency graph
     std::fs::write(depgraph_filename, dot.stdout).expect("failed to write Depgraph to output file");
+    Ok(())
 }
 
 fn cleanup_old_depgraphs() {
@@ -53,16 +52,23 @@ fn cleanup_old_depgraphs() {
 }
 
 ///Terminates process if 'depgraph' cmd is not installed
-fn is_depgraph_installed() {
-    let output = Command::new("cargo")
+fn are_build_deps_installed() -> Result<(), Box<dyn std::error::Error>> {
+    let depgraph = Command::new("cargo")
         .arg("depgraph")
-        .output()
-        .expect("'cargo depgraph' command missing")
+        .output()?
         .status
         .success();
 
-    if !output {
+    if !depgraph {
         eprint!("please install tool 'depgraph' https://github.com/jplatte/cargo-depgraph/\ncargo install cargo-depgraph");
         std::process::exit(1)
     }
+
+    let graphviz = Command::new("dot").arg("-V").output()?.status.success();
+
+    if !graphviz {
+        eprint!("please install tool 'graphviz' https://graphviz.org/download/");
+        std::process::exit(1)
+    }
+    Ok(())
 }
